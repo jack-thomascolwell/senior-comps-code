48a8548d613e3f44877f4915a77542b5b5c63db9
Jack Thomas-Colwell <jack.thomascolwell@gmail.com>
2022-11-15 17:02:42 -0800

    Fixed error where button/dropdown listeners were added twice

--- server/aom/static/aom/button.js
+++ server/aom/static/aom/button.js
@@ -1,8 +1,12 @@
 function addToggleCallbacks(parent) {
   Array.from(parent.querySelectorAll('.button.toggle')).forEach(b=> {
+    if (Boolean(b.dataset.hasListeners)) return;
+
     b.addEventListener('click', e => {
       b.classList.toggle('selected');
     });
+
+    b.dataset.hasListeners = true;
   });
 }
 
--- server/aom/static/aom/dropdown.js
+++ server/aom/static/aom/dropdown.js
@@ -1,21 +1,26 @@
 function addDropdownCallbacks(parent) {
   Array.from(parent.getElementsByClassName('dropdown')).forEach(d => {
     const f = (e => {
+      const target = !d.classList.contains('expanded');
       Array.from(document.getElementsByClassName('dropdown')).forEach(d2 => {
-        if (d2 != d)
-          d2.classList.remove('expanded');
+        d2.classList.remove('expanded');
       });
-      d.classList.toggle('expanded');
+      d.classList.toggle('expanded',target);
       e.stopPropagation();
     });
+
     if (d.classList.contains('persist')) Array.from(d.getElementsByTagName('li')).forEach(li => {
       li.addEventListener('click', e => {
         e.stopPropagation();
       });
     });
 
+    if (Boolean(d.dataset.hasListeners)) return;
+
     if (!d.classList.contains('discrete')) d.getElementsByClassName('dropdown_button')[0].addEventListener('click', f);
     d.getElementsByClassName('dropdown_arrow')[0].addEventListener('click', f);
+
+    d.dataset.hasListeners = true;
   });
 }
 
